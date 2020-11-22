b830be666d2fdafd5e23c4b708e4cbc40f79e53b
Christopher N. KATOYI <me@christopher2k.dev>
2020-11-22 21:00:24 +0100

    feat: calendar frames & structure

---
+++ src/components/Calendar/DayFrame.tsx
@@ -0,0 +1,107 @@
+import React, { FC } from 'react'
+import styled from '@emotion/styled'
+
+import { ReactComponent as DevelopIcon } from 'assets/icons/develop.svg'
+import { desktopStyle } from 'styles/responsive'
+
+const Root = styled.div`
+  --border-def: 1px solid ${props => props.theme.colors.black};
+  box-sizing: border-box;
+  width: 100%;
+  padding: 2rem ${props => props.theme.nav.padding.sides.mobile} 5rem;
+
+  border-bottom: var(--border-def);
+
+  ${desktopStyle`
+    border-right: var(--border-def);
+
+    &:nth-child(4n) {
+      border-right: none;
+    }
+  `}
+`
+
+const Top = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 100%;
+  margin-bottom: 2.5rem;
+`
+
+const Day = styled.span`
+  font-size: 5rem;
+  line-height: 5rem;
+  letter-spacing: -1.5px;
+`
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 1rem;
+  ${desktopStyle`display: none;`}
+`
+
+const CurrentImage = styled.img`
+  width: 100%;
+  height: auto;
+`
+
+const Bottom = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
+const Metadata = styled.div`
+  width: 60%;
+  flex-shrink: 0;
+
+  h1 {
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 2.7rem;
+  }
+
+  p {
+    text-transform: uppercase;
+    font-size: 1rem;
+    line-height: 1.7rem;
+  }
+`
+
+const CarouselDots = styled.div`
+  ${desktopStyle`display: none;`}
+`
+
+const Develop = styled(DevelopIcon)`
+  ${desktopStyle`display: none;`}
+`
+
+interface DayFrameProps {
+  article: Model.Article
+  dayNumber: string
+}
+
+export const DayFrame: FC<DayFrameProps> = ({ dayNumber, article }) => {
+  return (
+    <Root>
+      <Top>
+        <Day>{dayNumber}</Day>
+        <CarouselDots>Carousel DOTS</CarouselDots>
+      </Top>
+      <ImageWrapper>
+        <CurrentImage src={article.thumbnailPhoto} alt={article.title} />
+        <p>Caption</p>
+      </ImageWrapper>
+      <Bottom>
+        <Metadata>
+          <h1>{article.title}</h1>
+          <p>{article.categories.join(' | ')}</p>
+        </Metadata>
+        <Develop />
+      </Bottom>
+    </Root>
+  )
+}
--- src/pages/Inspirations/index.tsx
+++ src/pages/Inspirations/index.tsx
@@ -1,3 +1,49 @@
 import React, { FC } from 'react'
+import styled from '@emotion/styled'
 
-export const Inspirations: FC = () => <div>Inspirations</div>
+import { Calendar } from 'components/Calendar'
+import { desktopStyle } from 'styles/responsive'
+import { useWebsiteData } from 'hooks/useWebsiteData'
+
+const Root = styled.div`
+  width: 100%;
+  ${desktopStyle`
+    margin-top: 6rem;
+  `}
+`
+
+const Description = styled.p`
+  padding: 0 ${props => props.theme.nav.padding.sides.mobile};
+  font-family: ${props => props.theme.fonts.sneak};
+  font-size: 1.9rem;
+  line-height: 2.29rem;
+  text-align: left;
+
+  ${props => desktopStyle`
+    font-size: 3.1rem;
+    line-height: 4.03rem;
+    width: 50%;
+    padding: 0 ${props.theme.nav.padding.sides.desktop};
+  `};
+`
+
+export const Inspirations: FC = () => {
+  const { articles } = useWebsiteData()
+
+  return (
+    <Root>
+      <Description>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis
+        lorem sit amet pharetra feugiat. Vestibulum consectetur tristique leo,
+        eu ultricies leo pellentesque eu. Nam et nisi non enim sagittis
+        tincidunt. Curabitur ac orci eget orci malesuada volutpat. Morbi
+        ultrices lorem vel tempor vulputate. Sed lectus massa, convallis vitae
+        fringilla vel, facilisis nec magna. Maecenas ut augue non augue pulvinar
+        fringilla. Aliquam erat volutpat. Proin tristique id magna vel
+        pellentesque. Etiam eget magna vestibulum, semper sapien non, faucibus
+        ex. Pellentesque habitant morbi tristique senectus et netus
+      </Description>
+      {articles !== undefined && <Calendar articles={articles} />}
+    </Root>
+  )
+}
